41ff1c22c00af83f39a68a631365082f2533b280
Karthik V <karthikmftech@gmail.com>
2021-03-16 12:59:53 +0530

    My Second Commit, Pls push to cloud

---
+++ Py/Intro (1).py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+A = 10
+B = 15
+C = A*B
+print(C)
+print( 'The multiplication of {0} and {1} is {2}'. format(A,B,C))
+
---
+++ Py/Intro.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+10*20
+2220-123
+
+
+# In[3]:
+
+
+12-2
+
+
+# In[10]:
+
+
+A = 10
+B = 15
+C = A*B
+print(C)
+print( 'The multiplication of {0} and {1} is {2}'. format(A,B,C))
+
